47790b50bd666e599cf26a2813221a2f34c8727d
李洪森 <908207463@qq.com>
2017-10-22 23:46:13 +0800

    Initial commit

---
+++ src/main/java/com/example/teachereval/TeacherevalApplication.java
@@ -0,0 +1,12 @@
+package com.example.teachereval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeacherevalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeacherevalApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/teachereval/TeacherevalApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.teachereval;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TeacherevalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
